55b718c41443b6260440e31468f99e81c5acab3a
Fazil Niyazdeen TM <113542759+Fazilniyaz@users.noreply.github.com>
2025-05-24 14:25:35 +0530

    Update cartRoutes.js

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -11,16 +11,12 @@ const {
 } = require("../controllers/cartController");
 
 // Define routes
-router.route("/createCartItem").post(isAuthenticatedUsers, addItemIncart);
+router.route("/createCartItem").post(addItemIncart);
 // router.route("/updateCartItem/:id").put(isAuthenticatedUsers, updateItemInCart); // Changed to 'put' and added ':id' for clarity
-router
-  .route("/deleteCartItem/:id")
-  .delete(isAuthenticatedUsers, deleteItemInCart);
-router
-  .route("/updatePrice/:id")
-  .post(isAuthenticatedUsers, updateCartItemPrice);
+router.route("/deleteCartItem/:id").delete(deleteItemInCart);
+router.route("/updatePrice/:id").post(updateCartItemPrice);
 router
   .route("/CartProductsOfSingleUser/:userId")
-  .get(isAuthenticatedUsers, getAllCartItemsBySingleUser);
+  .get(getAllCartItemsBySingleUser);
 
 module.exports = router;
