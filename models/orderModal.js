f1d3a6818a36231d5245c0b6d8a67bf48a9ee05d
Fazil Niyazdeen TM <113542759+Fazilniyaz@users.noreply.github.com>
2025-07-07 19:13:28 +0530

    order Enhancement

--- models/orderModal.js
+++ models/orderModal.js
@@ -77,6 +77,13 @@ const orderSchema = mongoose.Schema({
     required: true,
     default: 0.0,
   },
+  noOfBottles: {
+    type: Number,
+    default: 1,
+  },
+  pricePerBottle: {
+    type: Number,
+  },
   paymentInfo: {
     id: {
       type: String,
